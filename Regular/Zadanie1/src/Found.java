247a16f9636af07ded7d12f4dd97870816cb35d2
nassstm <112973667+nassstm@users.noreply.github.com>
2022-09-29 20:04:15 +0300

    Add files via upload

---
+++ Regular/Zadanie1/src/Found.java
@@ -0,0 +1,17 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Found {
+
+     public static void Find(String wrd){
+        int count = 0;
+        //String wrd = "sssnn";
+        Pattern pattern = Pattern.compile("nn");
+        Matcher matcher = pattern.matcher(wrd);
+
+        while (matcher.find()){
+            count++;
+        }
+        System.out.println(count);
+    }
+}
---
+++ Regular/Zadanie2/src/Time.java
@@ -0,0 +1,19 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Time
+{
+    public static void Time(String input)
+
+    {
+
+        Pattern pattern = Pattern.compile("(2[0-3]|[0-1]\\d):[0-5]\\d");
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find())
+        {
+            System.out.println(matcher.group());
+        }
+
+    }
+
+}
